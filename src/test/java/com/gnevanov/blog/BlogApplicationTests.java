383fb201abcd0820c1d9289812eb0c10e8929f72
Egor <gnevanov.egor@gmail.com>
2020-05-11 18:34:29 +0300

    Initial commit

---
+++ src/test/java/com/gnevanov/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnevanov.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
